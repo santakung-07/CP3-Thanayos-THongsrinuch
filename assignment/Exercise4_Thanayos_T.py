3a079042bf6acead74f83bef759a65f7c64b1d40
santakung-07 <thanayos.yos@gmail.com>
2021-01-06 16:53:12 +0700

    Exercise4_Thanayos_T
    แบบฝึกหัดโปรแกรมเก็บคะแนน

---
+++ assignment/Exercise4_Thanayos_T.py
@@ -0,0 +1,12 @@
+# Exercise4 โปรแกรมเก็บคะแนน
+
+FE_score = 95.3         # คะแนน Foundation English
+GE_score = 80.0         # คะแนน General Business
+ICS_score = 82.6        # คะแนน Introduction to Computer Systems
+CP_score = 85.4         # คะแนน Computer Programming
+
+print("---Your score---")
+print(f"Foundation English คะแนนที่ได้: {FE_score} คะแนน")
+print(f"General Business คะแนนที่ได้: {GE_score} คะแนน")
+print(f"Introduction to Computer Systems คะแนนที่ได้: {ICS_score} คะแนน")
+print(f"Computer Programming คะแนนที่ได้: {CP_score} คะแนน")
