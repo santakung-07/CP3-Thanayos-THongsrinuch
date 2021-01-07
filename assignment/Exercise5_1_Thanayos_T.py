d17d24d9ca68020c825bb4a29c0a7893bc1e8b78
santakung-07 <thanayos.yos@gmail.com>
2021-01-07 14:46:26 +0700

    Exercise5_1
    การบ้าน +, -, *, / เลข 2 ตัวโดยรับ input

---
+++ assignment/Exercise5_1_Thanayos_T.py
@@ -0,0 +1,8 @@
+# exercise 5_1 +, -, *, / เลข 2 จำนวนโดยยรับ input
+print("Calculated 2 number\n","Results will be shown as +, -, *, / respectively")
+firstNum = int(input("First number: "))
+secondNum = int(input("Second number: "))
+print(f"{firstNum} + {secondNum} = {firstNum+secondNum}")
+print(f"{firstNum} - {secondNum} = {firstNum-secondNum}")
+print(f"{firstNum} * {secondNum} = {firstNum*secondNum}")
+print(f"{firstNum} / {secondNum} = {firstNum/secondNum}")
