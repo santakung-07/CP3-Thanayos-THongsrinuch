b9520afca19785e9ebc38afe2df62222fe4e8205
santakung-07 <thanayos.yos@gmail.com>
2021-01-10 13:12:13 +0700

    Lecture50_Thanayos_T
    Function
    +, -, *, /

---
+++ assignment/Lecture50_Thanayos_T.py
@@ -0,0 +1,18 @@
+# homework lecture 50
+def addNum(x,y):
+    print(x+y)
+
+
+def minusNum(x,y):
+    print(f"{x} - {y} = {x-y}")
+
+
+def multiplyNum(x,y):
+    print(x*y)
+
+
+def divisionNum(x,y):
+    if y == 0:
+        print("denominator can't be 0")
+    else:
+        print(x/y)
